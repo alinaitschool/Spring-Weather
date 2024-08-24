f41ad81b50280041141601465c7dfde253494db3
alinaberlin <alina.ghetler@gmail.com>
2024-08-24 02:54:17 +0200

    Initial commit

---
+++ src/main/java/com/itschool/projectweather/ProjectweatherApplication.java
@@ -0,0 +1,13 @@
+package com.itschool.projectweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectweatherApplication.class, args);
+	}
+
+}
